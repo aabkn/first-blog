0ff4ee48506f4dfd483aa295dd4fb080a87e2379
anastasiakireeva <anastasy.kireeva@gmail.com>
2017-02-06 22:07:21 +0300

    Comment with forms in post_detail without button add comment

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 def post_list(request):
@@ -10,7 +10,18 @@ def post_list(request):
 
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
-	return render(request, 'blog/post_detail.html', {'post': post})
+	if request.method == "POST":
+    		form = CommentForm(request.POST)
+    		if form.is_valid():
+    			comment = form.save(commit=False)
+    			comment.created_date = timezone.now()
+    			comment.post = post
+    			comment.save()
+    			return redirect('post_detail', pk=comment.post.pk)
+    	else:
+    		form = CommentForm()
+    	return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
+	#return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
 	if request.method == "POST":
@@ -24,3 +35,15 @@ def post_new(request):
 	else:
 		form = PostForm()
 	return render(request, 'blog/post_edit.html', {'form': form})
+
+'''def comment_new(request):
+	if request.method == "POST":
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save(commit=False)
+			post.created_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=comment.post.pk)
+	else:
+		form = CommentForm()
+	return render(request, 'blog/post_detail.html', {'form': form})'''
