9ec5b6fe4e1e79fa685f0f9177144cedb06824f3
anastasiakireeva <anastasy.kireeva@gmail.com>
2017-02-06 22:20:13 +0300

    Comment with forms in post_detail without button add comment and indent with spaces 5

--- blog/views.py
+++ blog/views.py
@@ -11,16 +11,16 @@ def post_list(request):
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == "POST":
-    		form = CommentForm(request.POST)
-    		if form.is_valid():
-    			comment = form.save(commit=False)
-    			comment.created_date = timezone.now()
-    			comment.post = post
-    			comment.save()
-    			return redirect('post_detail', pk=comment.post.pk)
-    else:
-    		form = CommentForm()
-    	return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
+	    form = CommentForm(request.POST)
+	    if form.is_valid():
+	        comment = form.save(commit=False)
+	        comment.created_date = timezone.now()
+	        comment.post = post
+	        comment.save()
+	        return redirect('post_detail', pk=comment.post.pk)
+	else:
+	    form = CommentForm()
+	return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
 	#return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
