8772de376f599e4c570335d0e20d69c62bd7232c
anastasiakireeva <anastasy.kireeva@gmail.com>
2017-03-22 19:43:09 +0300

    Authorization 2

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views
+
 
 urlpatterns = [
     # Examples:
@@ -8,4 +10,5 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 	url(r'', include('blog.urls')),
+	url(r'^accounts/login/$', views.login, name='login')
 ]
